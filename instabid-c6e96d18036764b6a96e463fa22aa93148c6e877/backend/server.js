357af92895c7962d8b602ecfa7f5039e0c752c96
SiteHypeInc <160051649+SiteHypeInc@users.noreply.github.com>
2025-12-31 01:33:51 -0500

    Update server.js

--- instabid-c6e96d18036764b6a96e463fa22aa93148c6e877/backend/server.js
+++ instabid-c6e96d18036764b6a96e463fa22aa93148c6e877/backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['[https://instabid.info](https://instabid.info)', '[http://instabid.info](http://instabid.info)', '[https://www.instabid.info](https://www.instabid.info)'],
+  origin: 'https://instabid.info', 'http://instabid.info', 'https://www.instabid.info',
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
