712addf91db46bc79860d7ca6aa20cb57a71cf97
SiteHypeInc <160051649+SiteHypeInc@users.noreply.github.com>
2025-12-31 01:59:30 -0500

    Update server.js
    nixed cors

--- instabid-c6e96d18036764b6a96e463fa22aa93148c6e877/backend/server.js
+++ instabid-c6e96d18036764b6a96e463fa22aa93148c6e877/backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
+/*const cors = require('cors');*/
 const { Pool } = require('pg');
 const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid-transport');
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({
+/*app.use(cors({
   origin: ['https://instabid.info', 'http://instabid.info', 'https://www.instabid.info'],
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -29,7 +29,21 @@ app.use((req, res, next) => {
   }
   
   next();
+});*/
+
+// Middleware - PERMISSIVE CORS FOR NOW
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', '*');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  next();
 });
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
