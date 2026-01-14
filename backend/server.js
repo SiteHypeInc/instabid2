4f46afe705f0eb307d04e86c173cc06e21c9f4e9
John Dudley <johndudley@Johns-MacBook-Air.local>
2026-01-14 13:42:22 -0500

    spelling

--- backend/server.js
+++ backend/server.js
@@ -997,7 +997,7 @@ app.post('/api/estimate', async (req, res) => {
         labor_hours, labor_rate, labor_cost,
         material_cost, equipment_cost, total_cost, photos,
         created_at
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, 17, NOW())
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, NOW())
       RETURNING id
     `;
 
